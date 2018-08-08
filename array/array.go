4b5734cb564be4876d55aa72a63f88fb84b0ca3d
chengy <chengy19930827@gmail.com>
2018-08-08 22:42:10 +0800

    加一

--- array/array.go
+++ array/array.go
@@ -162,3 +162,25 @@ func intersect(nums1 []int, nums2 []int) []int {
 	}
 	return data
 }
+
+//plusOne 给定一个非负整数组成的非空数组，在该数的基础上加一，返回一个新的数组。
+//
+// 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+//
+// 你可以假设除了整数 0 之外，这个整数不会以零开头。
+func plusOne(digits []int) []int {
+	length := len(digits)
+	for i := length - 1; i >= 0; i-- {
+		if digits[i]+1 == 10 {
+			digits[i] = 0
+		} else {
+			digits[i] = digits[i] + 1
+			return digits
+		}
+	}
+	//第一位为0 特殊处理
+	if digits[0] == 0 {
+		digits = append([]int{1}, digits...)
+	}
+	return digits
+}
